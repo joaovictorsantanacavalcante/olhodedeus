e5e2a178e87559c1be227c715d38c36a70efc3b6
joaovictorsantanacavalcante <108907098+joaovictorsantanacavalcante@users.noreply.github.com>
2022-07-09 22:35:38 -0300

    questoes ate alternativa 4 lista vetores e matrizes

---
+++ questao 2.py
@@ -0,0 +1,7 @@
+
+# comando para enverter os valores dentro dos vetores: 'nome do vetor'.recerse() em baixo coloque print e a variavel e ja ira estar revertida.
+vetor=[33, 1, 24, 37, 23, 55, 12, 35, 74, 19, 66]
+
+vetor.reverse()
+print(vetor)
+
